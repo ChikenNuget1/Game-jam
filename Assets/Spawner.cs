7ce0105165b34e1f2c6710e56290e116cb50dd4e
ChikenNuget1 <samuel.choi111@gmail.com>
2026-04-02 17:29:54 +1300

    Added randomised cat spawning on tileset coordinates
    Cats spawn randomly on the given tilemap only if the tile exists and no other cats have spawned there.

---
+++ Assets/Spawner.cs
@@ -0,0 +1,64 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Spawner : MonoBehaviour
+{
+
+    public GameObject cat;
+    public Tilemap tilemap;
+
+    public int spawnCount = 5;
+
+    // Store occupied cells
+    private HashSet<Vector3Int> occupiedCells = new HashSet<Vector3Int>();
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        spawn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void spawn()
+    {
+        for (int i = 0; i < spawnCount; i++)
+        {
+            SpawnRandomTile();
+        }
+        occupiedCells.Clear();
+    }
+
+    void SpawnRandomTile()
+    {
+        Vector3Int randomCell;
+        
+        // While cell doesn't exist or already contains a cat
+        do
+        {
+            randomCell = new Vector3Int(
+                Random.Range(tilemap.cellBounds.xMin, tilemap.cellBounds.xMax),
+                Random.Range(tilemap.cellBounds.yMin, tilemap.cellBounds.yMax),
+                0
+            );
+        } while (!tilemap.HasTile(randomCell) || occupiedCells.Contains(randomCell));
+
+        // Cell is occupied
+        occupiedCells.Add(randomCell);
+
+        Vector3 spawnPos = tilemap.GetCellCenterWorld(randomCell);
+
+        // Spawn object
+        GameObject obj = Instantiate(cat, spawnPos, Quaternion.identity);
+
+        // Layering order
+        SpriteRenderer sr = obj.GetComponent<SpriteRenderer>();
+        if (sr != null)
+            sr.sortingOrder = -(int)(spawnPos.y * 100);
+    }
+}
